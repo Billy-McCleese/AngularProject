84d17454e8435b003a1f7dd2287956632ea18894
Asma Razaq <asmarazaq@rocketmortgage.com>
2023-06-06 18:17:17 -0400

    made changes

--- StudyBuddySystem/Models/StudentBootCampDbContext.cs
+++ StudyBuddySystem/Models/StudentBootCampDbContext.cs
@@ -21,7 +21,7 @@ public partial class StudentBootCampDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.\\SQLExpress07;Database=StudentBootCampDB;Trusted_Connection=True; Encrypt=False;");
+        => optionsBuilder.UseSqlServer("Data Source=423Z1F3\\SQLExpress;Database=StudentBootCampDB;Trusted_Connection=True; Encrypt=False;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
